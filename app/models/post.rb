56e1cc024dbc76a8e2f081b90bf2ba967c6eccd3
Prashanth Pola <prashanth@codecrux.com>
2020-09-23 23:07:36 +0530

    removed unwanted code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
-class Post < ActiveRecord::Base
+# class Post < ActiveRecord::Base
 
-update_index('posts#post') { self }
+# update_index('posts#post') { self }
 
-	def self.search(search)
-		if search
-			@posts = Post.where(author: search)
-		else
-			@posts = Post.all
-		end
-	end
+# 	def self.search(search)
+# 		if search
+# 			@posts = Post.where(author: search)
+# 		else
+# 			@posts = Post.all
+# 		end
+# 	end
 
-end
+# end
